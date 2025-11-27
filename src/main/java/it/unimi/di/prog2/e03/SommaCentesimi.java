d895e6a6f58e537af158f894929186acd2463fa2
Nicolas Lampreda <nicolas.lampreda@studenti.unimi.it>
2025-11-27 12:37:47 +0000

    devcontainer build

--- src/main/java/it/unimi/di/prog2/e03/SommaCentesimi.java
+++ src/main/java/it/unimi/di/prog2/e03/SommaCentesimi.java
@@ -21,6 +21,8 @@ along with this file.  If not, see <https://www.gnu.org/licenses/>.
 
 package it.unimi.di.prog2.e03;
 
+import java.util.Scanner;
+
 /** Classe per la somma di importi in centesimi. */
 public class SommaCentesimi {
 
@@ -32,4 +34,21 @@ public class SommaCentesimi {
    * centesimi (uno per riga, con la parte decimale separata dalla parte intera da un punto) e ne
    * emetta nel flusso d'uscita la somma.
    */
+
+  /**
+   * Legge dal flusso di ingresso un elenco di importi in euro e centesimi (uno per riga, con la
+   * parte decimale separata dalla parte intera da un punto) e ne emette nel flusso d'uscita la
+   * somma.
+   *
+   * @param args gli argomenti (ignorati) della riga di comando
+  */
+  public static void main(String[] args) {
+    double totalSum = 0;
+    try(Scanner scanner = new Scanner(System.in)) {
+      while(scanner.hasNextDouble()) {
+        totalSum += scanner.nextDouble();
+      }
+      System.out.println(totalSum);
+    }
+  }
 }
